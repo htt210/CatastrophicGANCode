4a407e71dd53b34d4c0ca677ae1066fd4f180b23
Thanh Tung <hoangthanhtung53ca@gmail.com>
2019-07-24 00:59:02 +1000

    first commit

---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+python EWCGAN.py --ewcweight 10 --discount 0.9 --interval 200
+python EWCGAN.py --ewcweight 10 --discount 0.9 --interval 100
+python EWCGAN.py --ewcweight 10 --discount 0.9 --interval 10
+python EWCGAN.py --ewcweight 10 --discount 0.9 --interval 1
+
+python EWCGAN.py --ewcweight 10 --discount 0.95 --interval 200
+python EWCGAN.py --ewcweight 10 --discount 0.95 --interval 100
+python EWCGAN.py --ewcweight 10 --discount 0.95 --interval 10
+python EWCGAN.py --ewcweight 10 --discount 0.95 --interval 1
+
+python EWCGAN.py --ewcweight 10 --discount 0.99 --interval 200
+python EWCGAN.py --ewcweight 10 --discount 0.99 --interval 100
+python EWCGAN.py --ewcweight 10 --discount 0.99 --interval 10
+python EWCGAN.py --ewcweight 10 --discount 0.99 --interval 1
+
+python EWCGAN.py --ewcweight 20 --discount 0.9 --interval 200
+python EWCGAN.py --ewcweight 20 --discount 0.9 --interval 100
+python EWCGAN.py --ewcweight 20 --discount 0.9 --interval 10
+python EWCGAN.py --ewcweight 20 --discount 0.9 --interval 1
+
+python EWCGAN.py --ewcweight 20 --discount 0.95 --interval 200
+python EWCGAN.py --ewcweight 20 --discount 0.95 --interval 100
+python EWCGAN.py --ewcweight 20 --discount 0.95 --interval 10
+python EWCGAN.py --ewcweight 20 --discount 0.95 --interval 1
+
+python EWCGAN.py --ewcweight 20 --discount 0.99 --interval 200
+python EWCGAN.py --ewcweight 20 --discount 0.99 --interval 100
+python EWCGAN.py --ewcweight 20 --discount 0.99 --interval 10
+python EWCGAN.py --ewcweight 20 --discount 0.99 --interval 1
+
+python EWCGAN.py --ewcweight 5 --discount 0.9 --interval 200
+python EWCGAN.py --ewcweight 5 --discount 0.9 --interval 100
+python EWCGAN.py --ewcweight 5 --discount 0.9 --interval 10
+python EWCGAN.py --ewcweight 5 --discount 0.9 --interval 1
+
+python EWCGAN.py --ewcweight 5 --discount 0.95 --interval 200
+python EWCGAN.py --ewcweight 5 --discount 0.95 --interval 100
+python EWCGAN.py --ewcweight 5 --discount 0.95 --interval 10
+python EWCGAN.py --ewcweight 5 --discount 0.95 --interval 1
+
+python EWCGAN.py --ewcweight 5 --discount 0.99 --interval 200
+python EWCGAN.py --ewcweight 5 --discount 0.99 --interval 100
+python EWCGAN.py --ewcweight 5 --discount 0.99 --interval 10
+python EWCGAN.py --ewcweight 5 --discount 0.99 --interval 1
+
